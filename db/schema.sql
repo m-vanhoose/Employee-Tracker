b9e30e9c65ccec83bf9f5a19cc185c8a26020094
m-vanhoose <m-vanhoose@live.com>
2023-04-16 12:51:04 -0400

    filled in data for tables

--- db/schema.sql
+++ db/schema.sql
@@ -10,9 +10,11 @@ CREATE TABLE department (
 
 CREATE TABLE role (
     id INT PRIMARY KEY,
-    title VARCHAR(30),
+    title VARCHAR(50),
     salary DECIMAL,
     department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
 )
 
 CREATE TABLE employee (
@@ -21,4 +23,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
 )

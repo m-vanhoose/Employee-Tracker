a7ffe99480b7903f089da93dcb029d0c0250afe8
m-vanhoose <m-vanhoose@live.com>
2023-04-16 14:31:24 -0400

    fixed sql files

---
+++ db/query.sql
@@ -0,0 +1,8 @@
+
+USE Horiseon_db;
+
+SELECT * FROM department;
+
+SELECT * FROM role;
+
+SELECT * FROM employee;
--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,8 @@ USE Horiseon_db;
 
 CREATE TABLE department (
     id INT PRIMARY KEY,
-    name VARCHAR(30),
-)
+    name VARCHAR(30)
+);
 
 CREATE TABLE role (
     id INT PRIMARY KEY,
@@ -15,14 +15,15 @@ CREATE TABLE role (
     department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
-)
+);
 
 CREATE TABLE employee (
     id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT,
+    manager_id INT REFERENCES employee(id)  ON DELETE SET NULL,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
-)
+    
+);
